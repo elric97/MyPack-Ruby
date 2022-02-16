fa27787068ffdf0aa3b0c9afc3a9770a452b7c45
gkogant <gkogant@ncsu.edu>
2022-02-16 00:51:32 -0500

    Dealt with duplicate enrollment record

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -1,4 +1,6 @@
 class Enrollment < ApplicationRecord
   belongs_to :student
   belongs_to :course
+
+  validates :course_id, uniqueness: { scope: :student_id, message: 'record already exists' }
 end
