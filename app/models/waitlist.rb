131c0830207459dcb97bf980504e719723601fd2
gkogant <gkogant@ncsu.edu>
2022-02-16 20:47:11 -0500

    Changed the error message

--- app/models/waitlist.rb
+++ app/models/waitlist.rb
@@ -2,12 +2,12 @@ class Waitlist < ApplicationRecord
   belongs_to :student
   belongs_to :course
 
-  validates :course_id, uniqueness: { scope: :student_id, message: 'record already exists' }
+  validates :course_id, uniqueness: { scope: :student_id, message: 'Already waitlisted in this course' }
   validate :check_if_enrolled
 
   def check_if_enrolled
     @enrollment = Enrollment.where('course_id = ? AND student_id = ?', course_id, student_id)
-    errors.add(:course_id, 'Already enrolled') unless @enrollment.first.nil?
+    errors.add(:course_id, 'Already enrolled in this course') unless @enrollment.first.nil?
   end
 end
 
