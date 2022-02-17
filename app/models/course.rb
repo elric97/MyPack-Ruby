40f9834ffc23e427212b3416e3e3cad43d68b2eb
rsharm26 <rsharm26@ncsu.edu>
2022-02-16 21:21:21 -0500

    Fix course issue

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :instructor
-  has_many :enrollments
-  has_many :students, through: :enrollments, dependent: :destroy
+  has_many :enrollments, dependent: :destroy
+  has_many :students, through: :enrollments
 
-  has_many :waitlists
-  has_many :students, through: :waitlists, dependent: :destroy
+  has_many :waitlists, dependent: :destroy
+  has_many :students, through: :waitlists
 
   # has_many :waitlists
   validates :name, uniqueness: true, presence: true
