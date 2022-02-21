30a354e9dc346bfd7f25762d86335ee0bdba50e5
rsharm26 <rsharm26@ncsu.edu>
2022-02-21 16:35:46 -0500

    Test for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,7 +10,27 @@ class UserTest < ActiveSupport::TestCase
   test 'duplicate email' do
     user = User.new
     user.email = 'abcd@gmail.com'
-    user.password_digest =BCrypt::Password.create('secret')
+    user.password_digest = BCrypt::Password.create('secret')
+    user.name = 'Rachit'
+    assert_not user.save
+  end
+
+  test 'destroy user' do
+    assert users(:one).delete
+  end
+
+  test 'empty field user' do
+    user = User.new
+    user.email = ' '
+    user.password_digest = BCrypt::Password.create('secret')
+    user.name = ' '
+    assert_not user.save
+  end
+
+  test 'invalid email' do
+    user = User.new
+    user.email = 'abcdgmailcom'
+    user.password_digest = BCrypt::Password.create('secret')
     user.name = 'Rachit'
     assert_not user.save
   end
